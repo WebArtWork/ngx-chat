a41c651bd1e24c2f40dc0ab804e178618343ac3c
Vlad <58708136+rooben4ik@users.noreply.github.com>
2022-04-29 17:01:36 +0300

    Add files via upload

--- chat.module.ts
+++ chat.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CoreModule } from '@core';
-import { ChatComponent } from './chat.component';
+import { ChatComponent } from './chat/chat.component';
+import { MessengerComponent } from './messenger/messenger.component';
 
 @NgModule({
 	imports: [
 		CoreModule
 	],
 	declarations: [
+		MessengerComponent,
 		ChatComponent
 	],
 	exports: [
+		MessengerComponent,
 		ChatComponent
 	],
 	providers: []
 
 })
-
 export class ChatModule { }
