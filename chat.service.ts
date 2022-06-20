80ed8d0c57814f27d0d8944d22e08951eede3f76
Vlad <58708136+rooben4ik@users.noreply.github.com>
2022-06-20 13:32:10 +0300

    Add files via upload

--- chat.service.ts
+++ chat.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class ChatService {
-  constructor() { }
+	combine(_ids: string[], join = '') {
+		for (let i = 0; i < _ids.length; i++) {
+			_ids[i] = _ids[i].toString().substring(0, 8);
+			_ids[i] = new Date(parseInt(_ids[i], 16) * 1000).getTime().toString();
+		}
+		_ids.sort((a, b)=>{
+			if(Number(a) > Number(b)) {
+				return -1;
+			} else {
+				return 1;
+			}
+		});
+		return _ids.join(join);
+	}
+	constructor() { }
 }
