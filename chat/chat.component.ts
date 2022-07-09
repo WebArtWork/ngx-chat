a10c5cbbf0e339a026bfbc8c8b3aba435878ee7f
Honchar Denys <crackeraki@gmail.com>
2022-07-09 11:20:06 +0300

    Update chat.component.ts

--- chat/chat.component.ts
+++ chat/chat.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services';
-import { HttpService } from 'wacom';
+import { HttpService, SocketService } from 'wacom';
+import { ChatService } from '../chat.service';
 @Component({
 	selector: 'chat',
 	templateUrl: './chat.component.html',
@@ -32,14 +33,27 @@ export class ChatComponent implements OnInit {
 	}
 	constructor(
 		public us: UserService,
-		private http: HttpService
-	) {}
+		private http: HttpService,
+		private socket: SocketService,
+		public ch: ChatService
+	) {
+		socket.on('chat_message', created => {
+			if (created.chat === this.chat) {
+				if (this.isComment) {
+					this.chats.unshift(created);
+				} else {
+					this.chats.push(created);
+				}
+			}
+		});
+	}
 	public text = '';
 	send(){
 		this.http.post('/api/chat/send', {
 			chat: this.chat,
 			text: this.text
 		}, created => {
+			this.socket.emit('chat_message', created);
 			if (this.isComment) {
 				this.chats.unshift(created);
 			} else {
